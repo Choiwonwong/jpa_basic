2cd77be77202ac3cbf5b768c29978ed9f172ac43
cw2 <chldnjsdnd12@gmail.com>
2024-10-23 01:32:56 +0900

    [1022] Create Project & Setting JPA & test EntityManagerFactory and EntityManager

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member1 = new Member();
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
